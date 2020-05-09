5c3622aa65cd4555c290dab4dca25032a2fd4675
abbas007-ops <65022904+abbas007-ops@users.noreply.github.com>
2020-05-09 12:16:44 +0530

    Add files via upload

--- downloadcv.php
+++ downloadcv.php
@@ -19,9 +19,37 @@ while (!feof($fp)) {
   
 fclose($fp);*/
 
-$file_url = "https://abbas007-ops.github.io/mysite/Abbas'sResume(2).pdf";
-header('Content-Type: application/octet-stream');
-header("Content-Transfer-Encoding: Binary"); 
-header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\""); 
-readfile($file_url);   
+ignore_user_abort(true);
+set_time_limit(0); // disable the time limit for this script
+
+ // change the path to fit your websites document structure
+
+$dl_file = preg_replace("([^\w\s\d\-_~,;:\[\]\(\).]|[\.]{2,})", '', $_GET['file']); // simple file name validation
+$dl_file = filter_var($dl_file, FILTER_SANITIZE_URL); // Remove (more) invalid characters
+$fullPath = $dl_file;
+
+if ($fd = fopen ($fullPath, "r")) {
+    $fsize = filesize($fullPath);
+    $path_parts = pathinfo($fullPath);
+    $ext = strtolower($path_parts["extension"]);
+    switch ($ext) {
+        case "pdf":
+        header("Content-type: application/pdf");
+        header("Content-Disposition: attachment; filename=\"".$path_parts["basename"]."\""); // use 'attachment' to force a file download
+        break;
+        // add more headers for other content types here
+        default;
+        header("Content-type: application/octet-stream");
+        header("Content-Disposition: filename=\"".$path_parts["basename"]."\"");
+        break;
+    }
+    header("Content-length: $fsize");
+    header("Cache-control: private"); //use this to open files directly
+    while(!feof($fd)) {
+        $buffer = fread($fd, 2048);
+        echo $buffer;
+    }
+}
+fclose ($fd);
+exit;  
 ?> 
